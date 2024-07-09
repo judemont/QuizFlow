e3285fc4e557f52e8d16acb27ae7c4ab6d145113
judemont <julien@rmbi.ch>
2024-07-09 18:05:25 +0200

    Update: Add Voc model and DatabaseService to HomePage widget

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:voclearner/models/voc.dart';
+import 'package:voclearner/services/database.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +10,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Voc> vocs = [];
+
+  void loadVocs() {
+    DatabaseService.getVocs().then((value) {
+      setState(() {
+        vocs = value;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    loadVocs();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text("VocLearner"),
-    ));
+      appBar: AppBar(
+        title: const Text("VocLearner"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, index) {
+          return ListTile(
+            title: Text(vocs[index].title ?? ""),
+            subtitle: Text(vocs[index].description ?? ""),
+            onTap: () {},
+          );
+
+          // GestureDetector(
+          //     onTap: () {},
+          //     child: Card(
+          //         child: Padding(
+          //             padding: const EdgeInsets.all(8.0),
+          //             child: Column(
+          //               children: [
+          //                 Text(vocs[index].title ?? ""),
+          //                 Text(vocs[index].description ?? "")
+          //               ],
+          //             ))));
+        },
+        itemCount: vocs.length,
+      ),
+    );
   }
 }
