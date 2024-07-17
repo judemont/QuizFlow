e2738f471df9f1e34091b0712e67b841d4c529f9
judemont <julien@rmbi.ch>
2024-07-17 11:53:02 +0200

    added launcher icon

---
+++ lib/utilities/database.dart
@@ -0,0 +1,184 @@
+import 'dart:convert';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:quizflow/models/voc.dart';
+import 'package:quizflow/models/word.dart';
+
+class DatabaseService {
+  static const String databaseName = "quizflowDB.sqlite";
+  static Database? db;
+
+  static const databaseVersion = 1;
+  List<String> tables = ["Vocs", "Words"];
+
+  static Future<Database> initializeDb() async {
+    final databasePath = (await getApplicationDocumentsDirectory()).path;
+    final path = join(databasePath, databaseName);
+    return db ??
+        await openDatabase(
+          path,
+          version: databaseVersion,
+          onCreate: (Database db, int version) async {
+            await createTables(db);
+          },
+          onUpgrade: (db, oldVersion, newVersion) async {
+            await updateTables(db, oldVersion, newVersion);
+          },
+          onOpen: (db) async {
+            await openDB(db);
+          },
+        );
+  }
+
+  static openDB(Database db) {
+    db.rawQuery('SELECT * FROM sqlite_master ORDER BY name;').then((value) {
+      print(value);
+    });
+  }
+
+  static updateTables(Database db, int oldVersion, int newVersion) {
+    print(" DB Version : $newVersion");
+    print(oldVersion);
+    if (oldVersion < newVersion) {}
+  }
+
+  static Future<void> createTables(Database database) async {
+    await database.execute("""
+      CREATE TABLE Vocs(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT,
+          description TEXT
+      )
+    """);
+
+    await database.execute("""
+      CREATE TABLE Words(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          word TEXT,
+          definition TEXT,
+          vocId INTEGER
+      )
+    """);
+  }
+
+  static Future<int> createVoc(Voc voc) async {
+    print("CREATE");
+
+    final db = await DatabaseService.initializeDb();
+
+    final id = await db.insert('Vocs', voc.toMap());
+    return id;
+  }
+
+  static Future<int> createWord(Word word) async {
+    final db = await DatabaseService.initializeDb();
+
+    final id = await db.insert('Words', word.toMap());
+    return id;
+  }
+
+  static Future<List<Voc>> getVocs({String searchQuery = ""}) async {
+    final db = await DatabaseService.initializeDb();
+
+    List<Map<String, dynamic>> queryResult =
+        await db.query('Vocs', where: "title LIKE '%$searchQuery%'");
+
+    return queryResult.map((e) => Voc.fromMap(e)).toList();
+  }
+
+  static Future<List<Word>> getWord() async {
+    final db = await DatabaseService.initializeDb();
+
+    List<Map<String, dynamic>> queryResult = await db.query('Words');
+
+    return queryResult.map((e) => Word.fromMap(e)).toList();
+  }
+
+  static Future<List<Word>> getWordsFromVoc(int vocId) async {
+    final db = await DatabaseService.initializeDb();
+
+    List<Map<String, dynamic>> vocWords =
+        await db.query('Words', where: "vocId = $vocId");
+
+    return vocWords.map((e) => Word.fromMap(e)).toList();
+  }
+
+  static Future<void> removeVoc(int vocId) async {
+    final db = await DatabaseService.initializeDb();
+    db.delete("Vocs", where: "id = $vocId");
+    db.delete("Words", where: "vocId = $vocId");
+  }
+
+  static Future<void> removeWord(int wordId) async {
+    final db = await DatabaseService.initializeDb();
+    db.delete("Words", where: "id = $wordId");
+  }
+
+  static Future<void> removeWordsFromVoc(int vocId) async {
+    final db = await DatabaseService.initializeDb();
+    db.delete("Words", where: "vocId = $vocId");
+  }
+
+  static Future<void> updateVoc(Voc voc) async {
+    final db = await DatabaseService.initializeDb();
+    db.update("Vocs", voc.toMap(), where: 'id = ?', whereArgs: [voc.id]);
+  }
+
+  static Future<void> updateWord(Word word) async {
+    print("UPDATE");
+
+    final db = await DatabaseService.initializeDb();
+
+    db.update("Words", word.toMap(), where: 'id = ?', whereArgs: [word.id]);
+  }
+
+  Future<String> export({bool isEncrypted = false}) async {
+    print('GENERATE BACKUP');
+
+    var dbs = await DatabaseService.initializeDb();
+
+    List data = [];
+
+    List<Map<String, dynamic>> listMaps = [];
+
+    for (var i = 0; i < tables.length; i++) {
+      listMaps = await dbs.query(tables[i]);
+
+      data.add(listMaps);
+    }
+
+    List backups = [tables, data];
+
+    String jsonData = jsonEncode(backups);
+    print(jsonData);
+    return jsonData;
+  }
+
+  Future<void> import(String backup) async {
+    print(backup);
+    var dbs = await DatabaseService.initializeDb();
+
+    Batch batch = dbs.batch();
+
+    List jsonData = jsonDecode(backup);
+    print(jsonData);
+    List<Voc> actualVocs = await DatabaseService.getVocs();
+
+    for (var i = 0; i < jsonData[0].length; i++) {
+      for (var k = 0; k < jsonData[1][i].length; k++) {
+        if (actualVocs
+            .where((recipe) => recipe.title == jsonData[1][i][k]["title"])
+            .isEmpty) {
+          jsonData[1][i][k]["id"] = null;
+          batch.insert(jsonData[0][i], jsonData[1][i][k]);
+        }
+      }
+    }
+
+    await batch.commit(continueOnError: false, noResult: true);
+
+    print('RESTORE BACKUP');
+  }
+}
---
+++ lib/utilities/utils.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_selector/file_selector.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:quizflow/utilities/database.dart';
+
+import 'package:share_plus/share_plus.dart';
+
+class Utils {
+  static const Encoding _textFileEncoding = utf8;
+
+  static Future<void> userExport() async {
+    Directory directory = await getTemporaryDirectory();
+    String appDocumentsPath = directory.path;
+    String filePath = '$appDocumentsPath/QuizFlow_Export.json';
+
+    File file = File(filePath);
+
+    DatabaseService db = DatabaseService();
+    String result = await db.export();
+    var fileBytes = _textFileEncoding.encode(result);
+    await file.writeAsBytes(fileBytes);
+    await Share.shareXFiles([XFile(filePath)]);
+  }
+
+  static Future<int> userImport() async {
+    const XTypeGroup typeGroup = XTypeGroup(
+      label: 'QuizFlow export',
+      extensions: <String>['json'],
+    );
+    final XFile? file =
+        await openFile(acceptedTypeGroups: <XTypeGroup>[typeGroup]);
+
+    if (file != null) {
+      var fileBytes = await file.readAsBytes();
+      String backupContent = _textFileEncoding.decode(fileBytes);
+      DatabaseService db = DatabaseService();
+      await db.import(backupContent);
+    }
+    return 1;
+  }
+}
